d2a9e610a5af111b0b3a3d9ef4ff7f2ccc747127
Mars Cui <cuizhe@outlook.com>
2020-06-17 09:23:48 +0800

    text2speech
    use google gtts

---
+++ text2speech.py
@@ -0,0 +1,7 @@
+from gtts import gTTS
+# tts = gTTS(text='您好，您吃早饭了吗？需要我给你推荐些吃的吗？', lang='zh-tw')
+# tts.save("hello.mp3")
+
+with open ('news.txt', 'r', encoding="utf8") as f:
+	audio = gTTS(text=f.read(), lang="zh-cn")
+	audio.save("news.mp3")
