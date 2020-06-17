fbdd064df62bdfb030dbf3a83e1605d96d8d70c6
Mars Cui <cuizhe@outlook.com>
2020-06-17 09:33:20 +0800

    text 2 speech
    sample for text 2 speech

--- text2speech/text2speech.py
+++ text2speech/text2speech.py
@@ -1,7 +1,10 @@
 from gtts import gTTS
-# tts = gTTS(text='您好，您吃早饭了吗？需要我给你推荐些吃的吗？', lang='zh-tw')
-# tts.save("hello.mp3")
 
+# sample 1
+tts = gTTS(text='您好，您吃早饭了吗？需要我给你推荐些吃的吗？', lang='zh-tw')
+tts.save("hello.mp3")
+
+#sample 2
 with open ('news.txt', 'r', encoding="utf8") as f:
 	audio = gTTS(text=f.read(), lang="zh-cn")
 	audio.save("news.mp3")
